b8efae66e6fa99a4a10e60fb2feff140be4cd4ce
Akshay <akshaykhatri22@gmail.com>
2025-06-17 15:47:50 +0530

    Basic Node Setup

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log("MongoDB connection error:", err));
